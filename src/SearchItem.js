525d50864a86fa89e2ab52dbb24a4f135f23fb1b
Siva Sai Narayana Rao Akkipalli <115205629+narayanaongithub@users.noreply.github.com>
2023-01-11 09:17:47 +0530

    Update SearchItem.js

--- src/SearchItem.js
+++ src/SearchItem.js
@@ -1,6 +1,6 @@
 const SearchItem = ({ search, setSearch }) => {
     return (
-        <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+        <form autocomplete="off" className='searchForm' onSubmit={(e) => e.preventDefault()}>
             <label htmlFor='search'>Search</label>
             <input
                 id='search'
